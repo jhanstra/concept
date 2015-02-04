1999301d35a2a197e46c50806ebf4e662b0f7afa
Jared Hanstra <jhanstra@gmail.com>
2015-02-03 21:13:56 -0600

    labels, try to deploy to heroku, and components page

--- app/js/app.js
+++ app/js/app.js
@@ -11,9 +11,9 @@ app.controller('MainCtrl', ['$rootScope','$scope', function($rootScope, $scope) 
 // CONFIG / ROUTING
 app.config(function($routeProvider) {
   $routeProvider
-  .when('/', { templateUrl: 'views/concept.html' })
-  .when('/components', { templateUrl: 'views/components.html' })
-  .when('/css', {templateUrl: 'views/css.html'})
+  .when('/', { templateUrl: '/views/concept.html' })
+  .when('/components', { templateUrl: '/views/components.html' })
+  .when('/css', {templateUrl: '/views/css.html'})
   .otherwise({ redirectTo: '/' });
 
 });
