cfbcf7ab99d2ad864b01d53eb892852fc68a2a2a
Jared Hanstra <jhanstra@gmail.com>
2015-02-11 22:31:07 -0600

    need to commit so flightplan can work

---
+++ flightplan.js
@@ -0,0 +1,57 @@
+var plan = require('flightplan');
+
+var appName = 'concept';
+var username = 'deploy';
+var startFile = 'bin/www';
+
+var tmpDir = appName+'-' + new Date().getTime();
+
+// configuration
+plan.target('staging', [
+{
+  host: '104.131.93.214',
+  username: username,
+  agent: process.env.SSH_AUTH_SOCK
+}
+]);
+
+plan.target('production', [
+{
+  host: '104.236.144.86',
+  username: username,
+  agent: process.env.SSH_AUTH_SOCK
+},
+//add in another server if you have more than one
+// {
+//   host: '104.131.93.216',
+//   username: username,
+//   agent: process.env.SSH_AUTH_SOCK
+// }
+]);
+
+// run commands on localhost
+plan.local(function(local) {
+  // uncomment these if you need to run a build on your machine first
+  // local.log('Run build');
+  // local.exec('gulp build');
+
+  local.log('Copy files to remote hosts');
+  var filesToCopy = local.exec('git ls-files', {silent: true});
+  // rsync files to all the destination's hosts
+  local.transfer(filesToCopy, '/tmp/' + tmpDir);
+});
+
+// run commands on remote hosts (destinations)
+plan.remote(function(remote) {
+  remote.log('Move folder to root');
+  remote.sudo('cp -R /tmp/' + tmpDir + ' ~', {user: username});
+  remote.rm('-rf /tmp/' + tmpDir);
+
+  remote.log('Install dependencies');
+  remote.sudo('npm --production --prefix ~/' + tmpDir + ' install ~/' + tmpDir, {user: username});
+
+  remote.log('Reload application');
+  remote.sudo('ln -snf ~/' + tmpDir + ' ~/'+appName, {user: username});
+  remote.exec('forever stop ~/'+appName+'/'+startFile, {failsafe: true});
+  remote.exec('forever start ~/'+appName+'/'+startFile);
+});
